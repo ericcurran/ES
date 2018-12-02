347ed5925c7389caecccb1cffb99dbacb9b7ade9
Andriy Lakhno <andriy@lakhno.com>
2018-12-02 20:20:24 +0100

    Init Commit

---
+++ FolderMonitoring/FolderMonitoring.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace FolderMonitoring
+{
+    public static class FolderMonitoring
+    {
+        [FunctionName("FolderMonitoring")]
+        public static void Run([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
